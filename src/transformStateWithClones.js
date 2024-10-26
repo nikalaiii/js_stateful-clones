388a92b89623bdef09b2343c7dd876b36908bfc1
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2024-10-26 13:38:34 +0200

    finish code

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let currentState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      currentState = { ...currentState, ...actions[i].extraData };
+
+      result.push(currentState);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      const newState = { ...currentState };
+
+      for (const property of actions[i].keysToRemove) {
+        delete newState[property];
+      }
+      currentState = newState;
+      result.push(currentState);
+    }
+
+    if (actions[i].type === 'clear') {
+      currentState = {};
+      result.push(currentState);
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
